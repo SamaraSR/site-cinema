ac1f0950c1a42a3b356f8d1b00d11b1b7d3b77b7
samar_rocha <samara.rocha20@gmail.com>
2024-07-17 10:06:51 +0100

    Primeiro commit

---
+++ site-cinema/src/main/java/com/cinema/site_cinema/SiteCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.site_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteCinemaApplication.class, args);
+	}
+
+}
---
+++ site-cinema/src/test/java/com/cinema/site_cinema/SiteCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.site_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
