6c2219141cbb43dd29eee6425a94f3cd421aac24
samar_rocha <samara.rocha20@gmail.com>
2024-07-30 09:47:51 +0100

    implementação pag de erro

---
+++ site-cinema/src/main/java/com/cinema/site_cinema/config/CustomErrorConfig.java
@@ -0,0 +1,16 @@
+package com.cinema.site_cinema.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class CustomErrorConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error-404.html"));
+    }
+}
---
+++ site-cinema/src/main/java/com/cinema/site_cinema/controller/ErrorController.java
@@ -0,0 +1,13 @@
+package com.cinema.site_cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/error-404")
+    public String error404() {
+        return "error-404";
+    }
+}
